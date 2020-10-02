d9c1f7bdb82c215d11becef49e73d3f37ad5804c
jkelly97 <jmkelly2041@gmail.com>
2020-10-02 13:59:05 -0500

    Added webview.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -2,6 +2,10 @@ package edu.cnm.deepdive.animals.controller;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,18 +18,41 @@ import edu.cnm.deepdive.animals.model.ApiKey;
 import edu.cnm.deepdive.animals.service.AnimalService;
 import java.io.IOException;
 import java.util.List;
+
+import java.util.Objects;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
 public class ImageFragment extends Fragment {
 
+  private WebView contentView;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_image, container, false);
+    View root = inflater.inflate(R.layout.fragment_image, container, false);
+    setupWebView(root);
+    return root;
+  }
+
+  private void setupWebView(View root) {
+    contentView = root.findViewById(R.id.content_view);
+    contentView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+      }
+    });
+    WebSettings settings = contentView.getSettings();
+    settings.setJavaScriptEnabled(true);
+    settings.setSupportZoom(true);
+    settings.setBuiltInZoomControls(true);
+    settings.setDisplayZoomControls(false);
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
+    new Retriever().start();
   }
 
   private class Retriever extends Thread {
@@ -34,7 +61,9 @@ public class ImageFragment extends Fragment {
     public void run() {
 
       Gson gson = new GsonBuilder()
+
           .create();
+
       Retrofit retrofit = new Retrofit.Builder()
           .baseUrl("https://us-central1-apis-4674e.cloudfunctions.net/")
           .addConverterFactory(GsonConverterFactory.create(gson))
@@ -46,18 +75,26 @@ public class ImageFragment extends Fragment {
 
         Response<ApiKey> keyResponse = animalService.getApiKey().execute();
         ApiKey key = keyResponse.body();
+        assert key != null;
         final String clientKey = key.getKey();
 
         Response<List<Animals>> listResponse = animalService.getAnimals(clientKey).execute();
-        List<Animals> animalsList = listResponse.body();
-        final String imageUrl = animalsList.get(0).getImageUrl();
+        List<Animals> animalList = listResponse.body();
+        assert animalList != null;
+        final String imageUrl = animalList.get(43).getImageUrl();
+
+        Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            contentView.loadUrl(imageUrl);
+          }
+        });
 
       } catch (IOException e) {
         Log.e("AnimalService", e.getMessage(), e);
-
       }
 
-
     }
+
   }
 }
--- app/src/main/java/edu/cnm/deepdive/animals/model/Animals.java
+++ app/src/main/java/edu/cnm/deepdive/animals/model/Animals.java
@@ -3,65 +3,67 @@ package edu.cnm.deepdive.animals.model;
 import com.google.gson.annotations.SerializedName;
 
 public class Animals {
+
   private String name;
   private Taxonomy taxonomy;
   private String location;
   private Speed speed;
   private String diet;
   private String lifespan;
+
+  @SerializedName("image")
   private String imageUrl;
 
   public String getName() {
     return name;
   }
 
-  public Taxonomy getTaxonomy() {
-    return taxonomy;
-  }
-
-  public String getLocation() {
-    return location;
-  }
-
-  public Speed getSpeed() {
-    return speed;
-  }
-
-  public String getDiet() {
-    return diet;
-  }
-
-  public String getLifespan() {
-    return lifespan;
-  }
-
-  public String getImageUrl() {
-    return imageUrl;
-  }
-
-  @SerializedName("image")
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Taxonomy getTaxonomy() {
+    return taxonomy;
   }
 
   public void setTaxonomy(Taxonomy taxonomy) {
     this.taxonomy = taxonomy;
   }
 
+  public String getLocation() {
+    return location;
+  }
+
   public void setLocation(String location) {
     this.location = location;
+  }
+
+  public Speed getSpeed() {
+    return speed;
   }
 
   public void setSpeed(Speed speed) {
     this.speed = speed;
   }
 
+  public String getDiet() {
+    return diet;
+  }
+
   public void setDiet(String diet) {
     this.diet = diet;
   }
 
+  public String getLifespan() {
+    return lifespan;
+  }
+
   public void setLifespan(String lifespan) {
     this.lifespan = lifespan;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
   }
 
   public void setImageUrl(String imageUrl) {
