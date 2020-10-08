3b4ba857cc47b347e5771972a74a6c83142be700
jkelly97 <jmkelly2041@gmail.com>
2020-10-08 12:02:14 -0500

    Implemented ViewModel.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -7,11 +7,15 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.collection.CircularArray;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.animals.BuildConfig;
@@ -19,6 +23,7 @@ import edu.cnm.deepdive.animals.R;
 import edu.cnm.deepdive.animals.model.Animals;
 import edu.cnm.deepdive.animals.model.ApiKey;
 import edu.cnm.deepdive.animals.service.AnimalService;
+import edu.cnm.deepdive.animals.viewmodel.AnimalViewModel;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
@@ -30,6 +35,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ImageFragment extends Fragment {
 
   private WebView contentView;
+  private AnimalViewModel animalViewModel;
+
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +44,21 @@ public class ImageFragment extends Fragment {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
     setupWebView(root);
     return root;
+  }
+
+
+  @Override
+  public void onViewCreated(@NonNull View view,
+      @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    animalViewModel = new ViewModelProvider(getActivity())
+        .get(AnimalViewModel.class);
+    animalViewModel.getAnimals().observe(getViewLifecycleOwner(), new Observer<List<Animals>>() {
+      @Override
+      public void onChanged(List<Animals> animals) {
+        contentView.loadUrl(animals.get(22).getImageUrl());
+      }
+    });
   }
 
   private void setupWebView(View root) {
@@ -47,6 +69,8 @@ public class ImageFragment extends Fragment {
         return false;
       }
     });
+
+
     WebSettings settings = contentView.getSettings();
     settings.setJavaScriptEnabled(true);
     settings.setSupportZoom(true);
@@ -54,52 +78,8 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
-    new RetrieverTask().execute();
   }
 
 
-  private class RetrieverTask extends AsyncTask<Void, Void, List<Animals>> {
-
-    private AnimalService animalService;
-
-    @Override
-    protected void onPreExecute() {
-      super.onPreExecute();
-      Gson gson = new GsonBuilder()
-          .create();
-      Retrofit retrofit = new Retrofit.Builder()
-          .baseUrl(BuildConfig.BASE_URL)
-          .addConverterFactory(GsonConverterFactory.create(gson))
-          .build();
-      animalService = retrofit.create(AnimalService.class);
-    }
-
-    @Override
-    protected List<Animals> doInBackground(Void... voids) {
-      try {
-        Response<ApiKey> keyResponse = animalService.getApiKey().execute();
-        ApiKey key = keyResponse.body();
-        assert key != null;
-        final String clientKey = key.getKey();
-
-        Response<List<Animals>> listResponse = animalService.getAnimals(clientKey).execute();
-        List<Animals> animalList = listResponse.body();
-        assert animalList != null;
-        return animalList;
-
-      } catch (IOException e) {
-        Log.e("AnimalService", e.getMessage(), e);
-        cancel(true);
-      }
-      return null;
-    }
-
-    @Override
-    protected void onPostExecute(List<Animals> animalsList) {
-      Random rng = new Random();
-      final String imageUrl = animalsList.get(rng.nextInt(animalsList.size())).getImageUrl();
-      contentView.loadUrl(imageUrl);
-    }
-  }
 
 }
